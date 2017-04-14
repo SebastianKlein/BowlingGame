f68cb0487e19279c0beeb10e93bd6e340233db69
Sebastian.Klein <Klein_Sebastian@gmx.de>
2017-04-14 13:06:29 +0200

    added bowling game constructor

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class BowlingGame {
 
     private List<Player> players;
-    public BowlingGame(String ... players) {
+    public BowlingGame(String ... playerNames) {
+        this.players = new ArrayList<Player>();
+
+        for(String pn: playerNames)
+        players.add(new Player(pn));
     }
 
     public List<Player> getPlayers() {
-        return player;
+        return players;
     }
 }
