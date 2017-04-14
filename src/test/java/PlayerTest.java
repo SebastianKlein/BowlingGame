01ba73f58c1b860ee99b7072917d732daf2f7320
Gauti Nils <gauti.bernhardsson@valtech.com>
2017-04-14 13:05:33 +0200

    One player Test added

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,4 +1,13 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 public class PlayerTest {
 
-
+    @Test
+    public void create_player(){
+        Player p = new Player("Bisu");
+        assertThat(p.getName(), equalTo("Bisu"));
+    }
 }
